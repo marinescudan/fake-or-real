54186f8b97e355764e6b539b74cfec11d095e93b
Dan Marinescu <marinescu.dan.r@gmail.com>
2020-05-27 02:09:05 +0200

    WIP - fixes question overflow

--- src/components/media/cFigure.vue
+++ src/components/media/cFigure.vue
@@ -1,6 +1,7 @@
 <template>
     <figure class="c-figure aspect-ratio aspect-ratio--16x9">
         <img :src="src" :alt="alt" :title="title" :width="width" :height="height"/>
+        <img v-bind:src="image_media_url(src)"/>
     </figure>
 </template>
 
@@ -12,23 +13,30 @@ export default {
       type: String,
       required: true
     },
-    alt: {
-      type: String,
-      required: false
+    // alt: {
+    //   type: String,
+    //   required: false
+    // },
+    // title: {
+    //   type: String,
+    //   required: false
+    // },
+    // width: {
+    //   type: Number,
+    //   required: false
+    // },
+    // height: {
+    //   type: Number,
+    //   required: false
+    // }
+  },
+  methods: {
+    image_media_url: function (url) {
+      let arr  = url.split('/');
+      let src = arr[arr.length - 1].split('.')[0];
+      return `./img/questions/square/${src}.jpg`;
     },
-    title: {
-      type: String,
-      required: false
-    },
-    width: {
-      type: Number,
-      required: false
-    },
-    height: {
-      type: Number,
-      required: false
-    }
-  }
+  },
 }
 </script>
 
@@ -40,26 +48,26 @@ export default {
   display: block
   margin: 0
   position: relative
-  @include imageBackground
+  // @include imageBackground
   img
-    position: absolute
+    // position: absolute
     width: 100%
     height: 100%
-    top: 0
-    left: 0
-    right: 0
-    bottom: 0
+    // top: 0
+    // left: 0
+    // right: 0
+    // bottom: 0
 
-.selected
-  background: rgba(255, 255, 255, 1)
-  .c-figure::before
-    content: ""
-    position: absolute
-    top: 0
-    right: 0
-    bottom: 0
-    left: 0
-    @include selectedBackground
-    z-index: 3
+// .selected
+//   background: rgba(255, 255, 255, 1)
+//   .c-figure::before
+//     content: ""
+//     position: absolute
+//     top: 0
+//     right: 0
+//     bottom: 0
+//     left: 0
+//     @include selectedBackground
+//     z-index: 3
 
 </style>
