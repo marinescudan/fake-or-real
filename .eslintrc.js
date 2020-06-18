0f2d9907a21f013ae9ee1a29d40c7624771c0c2e
Dan Marinescu <marinescu.dan.r@gmail.com>
2020-06-18 15:45:06 +0200

    Remove jslint rull

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,8 +12,7 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    "comma-dangle": ["error", "never"]
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   overrides: [
     {
